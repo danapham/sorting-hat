6321b403fbdcf4c26347e7e7ff08944e97e079bd
danapham <danapham12@gmail.com>
2020-08-01 12:38:53 -0500

    build cards with added student

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 const studentCards = [
-{
-  name: 'Ginny Weasley',
-  house: 'Gryffindor',
-},
-{
-  name: 'Ginny Weasley',
-  house: 'Gryffindor',
-},
-{
-  name: 'Ginny Weasley',
-  house: 'Gryffindor',
-},
-{
-  name: 'Ginny Weasley',
-  house: 'Gryffindor',
-}]
+  {
+    name: "Ginny Weasley",
+    house: "Gryffindor",
+  },
+  {
+    name: "Ginny Weasley",
+    house: "Gryffindor",
+  },
+  {
+    name: "Ginny Weasley",
+    house: "Gryffindor",
+  },
+  {
+    name: "Ginny Weasley",
+    house: "Gryffindor",
+  },
+];
 
-const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+
+const buttonEvents = () => {
+  document.getElementById("jumbotron-btn").addEventListener("click", buildForm);
+
+};
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
-}
+};
 
 const buildForm = () => {
-  let domString = '';
+  let domString = "";
 
-  domString += `<h2>Enter First Year's Name</h2>`
-  domString += `<form class="form-inline">
+  domString += `<h2>Enter First Year's Name</h2>`;
+  domString += `<form class="form-inline" id="whole-form">
                   <div class="form-group mb-2">
                     <label for="staticEmail2" class="sr-only">Student</label>
                     <input type="text" readonly class="form-control-plaintext" id="staticStudent" value="Student:">
@@ -36,45 +42,38 @@ const buildForm = () => {
                    <label for="inputPassword2" class="sr-only">Student Name</label>
                    <input type="text" class="form-control" id="inputStudent" placeholder="Hermione Granger">
                   </div>
-                    <button type="submit" class="btn btn-primary mb-2" id="form-btn">Sort!</button>
-                </form>`
+                  <button type="submit" class="btn btn-primary mb-2" id="form-btn">Sort!</button>
+                </form>`;
 
-printToDom('form', domString);
+  printToDom("form", domString);
+
+  document.getElementById('form-btn').addEventListener('click', addStudent);
+  document.getElementById("form-btn").addEventListener("click", buildCards);
+};
+
+const addStudent = () => {
+  const randomHouse = houses[Math.floor(Math.random() * houses.length)];
+  const newStudent = {
+    name: document.getElementById("inputStudent").value,
+    house: randomHouse,
+  }
+  studentCards.push(newStudent)
 }
 
 const buildCards = () => {
-//   let targetId = e.target.id
 
-//   if (targetId === 'form-btn') {
+  let domString = "";
 
-//   const randomHouse = houses[Math.floor(Math.random() * houses.length)];
-
-//   let domString = '';
-
-//   domString += `<div class="card" style="width: 18rem;">
-//                   <div class="card-body">
-//                     <h5 class="card-title">inputStudent</h5>
-//                     <p class="card-text">${randomHouse}</p>
-//                     <a href="#" class="btn btn-primary">Expel</a>
-//                   </div>
-//                 </div>`
-
-// printToDom('student-cards', domString);
-console.log('it worked');
-}
-
-const jumboEvent = () => {
-  document.getElementById('jumbotron-btn').addEventListener('click', buildForm);
-}
-
-jumboEvent();
-
-const formEvent = () => {
-  const cardBuilding = document.querySelector('#formBtn');
-
-  if (cardBuilding) {
-    document.getElementById('form-btn').addEventListener('click', buildCards);
+  for (let i = 0; i < studentCards.length; i++) {
+    domString += `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${studentCards[i].name}</h5>
+                      <p class="card-text">${studentCards[i].house}</p>
+                      <a href="#" class="btn btn-primary">Expel</a>
+                    </div>
+                  </div>`;
   }
-}
+    printToDom("student-cards", domString);
+};
 
-formEvent();
+buttonEvents();
